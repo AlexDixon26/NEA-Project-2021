2a519ff83a437ccace318f7f0d65834340a1e298
Alex Dixon <alexanderdixon267@gmail.com>
2021-07-31 10:34:06 +0200

    Changed GUI to be inside Ui

---
+++ UI.py
@@ -0,0 +1,40 @@
+from Game import Game
+from sys import argv
+
+class GUI:
+    def __init__(self):
+        self.__game = Game() #Game.Ai/Human/Client,Game.Ai/Human/Client in brackets
+
+    def run(self):
+        pass
+
+class Terminal:
+    def __init__(self):
+        self.__game = Game(Game.Human,Game.Human)
+
+
+    def run(self):
+        print("PLAY (Press Any Button to continue)")
+        _ = input("")
+
+
+
+
+def usage():   
+    print(f"""
+Usage: {argv[0]} [g | t]
+g : play with the GUI
+t : play with the Terminal""")
+    quit()
+
+if __name__ == "__main__":
+    if len(argv) != 2:
+        usage()
+    elif argv[1] == "t":
+        ui = Terminal()
+    elif argv[1] == "g":
+        ui = GUI()
+    else:
+        usage()
+    
+    ui.run()
