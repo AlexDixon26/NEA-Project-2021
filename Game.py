c6e27eb4fd85dcc8e7493c5a1e2448f88381df59
Alex Dixon <alexanderdixon267@gmail.com>
2021-07-31 11:21:41 +0200

    repr for Game class complete

--- Game.py
+++ Game.py
@@ -29,11 +29,12 @@ class Game:
             self.__player2 = Human()
         elif player2 == Game.Network:
             self.__player2 = Client()
-        self.board = [[Game._EMPTY for _ in range(Game._DIM)] for _ in range(Game._DIM)]
-        self.__player = Game.P1
+        self._board = [[Game._EMPTY for _ in range(Game._DIM)] for _ in range(Game._DIM)]
+        self._player = Game.P1
         
     def do_move(self, row, col):
-        row, col -= 1, 1 #row & column entered will be 1-based, code will use 0-based
+        row -= 1
+        col -= 1 #row & column entered will be 1-based, code will use 0-based
         if self._board[row][col] != Game._EMPTY:
             raise GameError("Invalid Move")
         self._board[row][col] = self._player
@@ -46,9 +47,18 @@ class Game:
             finished = True
             for row in range(Game._DIM):
                 for col in range(Game._DIM):
-                    if self.board[row][col] == p:
+                    if self._board[row][col] == p:
                         finished = False
             if finished == True:
                 return True
         return None
         #Do Draw Mechanics later
+    
+    def __repr__(self):
+        result = "  " + " ".join(str(i+1) for i in range(Game._DIM))
+        for row in range(Game._DIM):
+            result += f"\n{row+1} " + "|".join(self._board[row])
+            if row != Game._DIM - 1:
+                dashes = "-" * (2 * Game._DIM - 1)
+                result += f"\n  {dashes}"
+        return result
