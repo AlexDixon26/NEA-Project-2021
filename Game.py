8b9e34a6f44645d730d96ec23dc177ea85805b02
Alex Dixon <alexanderdixon267@gmail.com>
2021-08-05 16:17:18 +0200

    Fixed bugs + Added capturing piece

--- Game.py
+++ Game.py
@@ -35,90 +35,290 @@ class Game:
             self.__player2 = Human()
         elif player2 == Game.Network:
             self.__player2 = Client()
-        self._board = [[Game._EMPTY,Game.P2,Game._EMPTY,Game.P2,Game._EMPTY,Game.P2,Game._EMPTY,Game.P2],[Game.P2,Game._EMPTY,Game.P2,Game._EMPTY,Game.P2,Game._EMPTY,Game.P2,Game._EMPTY],[Game._EMPTY,Game.P2,Game._EMPTY,Game.P2,Game._EMPTY,Game.P2,Game._EMPTY,Game.P2],[Game._EMPTY]*Game._DIM,[Game._EMPTY]*Game._DIM,[Game.P1,Game._EMPTY,Game.P1,Game._EMPTY,Game.P1,Game._EMPTY,Game.P1,Game._EMPTY],[Game._EMPTY,Game.P1,Game._EMPTY,Game.P1,Game._EMPTY,Game.P1,Game._EMPTY,Game.P1],[Game.P1,Game._EMPTY,Game.P1,Game._EMPTY,Game.P1,Game._EMPTY,Game.P1,Game._EMPTY]]
+        self._board = [[Game._EMPTY,Game.P2Man,Game._EMPTY,Game._EMPTY,Game._EMPTY,Game._EMPTY,Game._EMPTY,Game._EMPTY],[Game._EMPTY,Game._EMPTY,Game._EMPTY,Game._EMPTY,Game._EMPTY,Game._EMPTY,Game._EMPTY,Game._EMPTY],[Game._EMPTY,Game._EMPTY,Game._EMPTY,Game._EMPTY,Game._EMPTY,Game._EMPTY,Game._EMPTY,Game._EMPTY],[Game._EMPTY]*Game._DIM,[Game._EMPTY]*Game._DIM,[Game.P1Man,Game._EMPTY,Game._EMPTY,Game._EMPTY,Game._EMPTY,Game._EMPTY,Game._EMPTY,Game._EMPTY],[Game._EMPTY,Game._EMPTY,Game._EMPTY,Game._EMPTY,Game._EMPTY,Game._EMPTY,Game._EMPTY,Game._EMPTY],[Game._EMPTY,Game._EMPTY,Game._EMPTY,Game._EMPTY,Game._EMPTY,Game._EMPTY,Game._EMPTY,Game._EMPTY]]
         self._player = Game.P1
         
-    def _do_move(self, row, col, row_to_move, col_to_move):
+    def _do_move(self, row, col, row_to_move, col_to_move, take_used):
+        #Validation of piece being theres
+        if self._player == Game.P1:
+            possible = [Game.P1King,Game.P1Man]
+        else: 
+            possible = [Game.P2King,Game.P2Man]
         row -= 1
         col -= 1
+        if self._board[row][col] not in possible:
+            raise GameError("Not your piece")
         row_to_move -= 1
         col_to_move -= 1
         if self._board[row][col] == Game.P1Man or self._board[row][col] == Game.P2Man:
             man_or_king = "S"
         else:
             man_or_king = "K"
-        if self._board[row][col] != Game._EMPTY:
-            self._board[row][col] = Game._EMPTY
-            if man_or_king == "S":
-                self._board[row_to_move][col_to_move] = Game.P2Man if self._player is Game.P1 else Game.P1Man
-            else:
-                self._board[row_to_move][col_to_move] = Game.P2King if self._player is Game.P1 else Game.P1King
+        if take_used == False:
+            if self._board[row][col] != Game._EMPTY:
+                self._board[row][col] = Game._EMPTY
+                if man_or_king == "S":
+                    self._board[row_to_move][col_to_move] = Game.P1Man if self._player is Game.P1 else Game.P2Man
+                else:
+                    self._board[row_to_move][col_to_move] = Game.P1King if self._player is Game.P1 else Game.P2King
+        else:
+             if self._board[row][col] != Game._EMPTY:
+                self._board[row][col] = Game._EMPTY
+                if man_or_king == "S":
+                    self._board[row_to_move][col_to_move] = Game.P1Man if self._player is Game.P1 else Game.P2Man
+                    self._board[int((row_to_move + row)/2)][int((col_to_move + col)/2)] = Game._EMPTY
+                else:
+                    self._board[row_to_move][col_to_move] = Game.P1King if self._player is Game.P1 else Game.P2King
+                    self._board[int((row_to_move + row)/2)][int((col_to_move + col)/2)] = Game._EMPTY
+        self.check_for_new_king()
+        if self._player == Game.P1:
+            self._player = Game.P2
+        else:
+            self._player = Game.P1
         
     def check_for_new_king(self):
-           
+        for row in range(Game._DIM):
+                for col in range(Game._DIM):
+                    if self._player == Game.P1:
+                        if col == 8:
+                            if self._board[row][col] in [Game.P1Man]:
+                                self._board[row][col] == Game.P1King
+                    else:
+                        if col == 1:
+                            if self._board[row][col] in [Game.P2Man]:
+                                self._board[row][col] == Game.P2King
+
 
     def _get_legal_moves(self, row_of_piece, col_of_piece):
-        moves = []
-        not_player = Game.P1 if self._player is Game.P1 else Game.P2
-        if self._board[row_of_piece-1][col_of_piece-1] == Game._EMPTY or  self._board[row_of_piece-1][col_of_piece-1] == not_player:
-            return -1
-        if self._board[row][col] == Game.P1Man or self._board[row][col] == Game.P2Man:
-            man_or_king = "S"
+        if self._player == Game.P1:
+            possible = [Game.P1King,Game.P1Man] 
+        else: 
+            possible = [Game.P2King,Game.P2Man]
+        if self._board[row_of_piece-1][col_of_piece-1] not in possible:
+            raise GameError("Not your piece")
+        take = 0
+        result = []
+        row_of_piece -= 1
+        col_of_piece -= 1
+        if self._player == Game.P1:
+            row = row_of_piece - 1
+            if col_of_piece == 0:
+                col = col_of_piece + 1
+                if self._board[row][col] in [Game.P1Man,Game.P1King]:
+                    result = result
+                elif self._board[row][col] == Game._EMPTY:
+                    result.append([row,col])
+                else:
+                    if self._board[row-1][col+1] == Game._EMPTY:
+                        result.append([row-1,col+1])
+                        take += 1
+                    else:
+                        result = result
+            elif col_of_piece == 7:
+                col = col_of_piece - 1
+                if self._board[row][col] in [Game.P1Man,Game.P1King]:
+                    result = result
+                elif self._board[row][col] == Game._EMPTY:
+                    result.append([row,col])
+                else:
+                    if self._board[row-1][col-1] == Game._EMPTY:
+                        result.append([row-1,col-1])
+                        take += 1
+                    else:
+                        result = result
+            elif col_of_piece in [1,2,3,4,5,6]:
+                col = col_of_piece - 1
+                if self._board[row][col] in [Game.P1Man,Game.P1King]:
+                    result = result
+                elif self._board[row][col] == Game._EMPTY:
+                    result.append([row,col])
+                else:
+                    if self._board[row-1][col-1] == Game._EMPTY:
+                        result.append([row-1,col-1])
+                        take += 1
+                    else:
+                        result = result
+                col = col_of_piece + 1
+                if self._board[row][col] in [Game.P1Man,Game.P1King]:
+                    result = result
+                elif self._board[row][col] == Game._EMPTY:
+                    result.append([row,col])
+                else:
+                    if self._board[row-1][col+1] == Game._EMPTY:
+                        result.append([row-1,col+1])
+                        take += 1
+                    else:
+                        result = result
+            if self._board[row][col] == Game.P1King:
+                row = row_of_piece + 1
+                if col_of_piece == 0:
+                    col = col_of_piece + 1
+                    if self._board[row][col] in [Game.P2Man,Game.P2King]:
+                        result = result
+                    elif self._board[row][col] == Game._EMPTY:
+                        result.append([row,col])
+                    else:
+                        if self._board[row-1][col+1] == Game._EMPTY:
+                            result.append([row-1,col+1])
+                            take += 1
+                        else:
+                            result = result
+                elif col_of_piece == 7:
+                    col = col_of_piece + 1
+                    if self._board[row][col] in [Game.P2Man,Game.P2King]:
+                        result = result
+                    elif self._board[row][col] == Game._EMPTY:
+                        result.append([row,col])
+                    else:
+                        if self._board[row-1][col-1] == Game._EMPTY:
+                            result.append([row-1,col-1])
+                            take += 1
+                        else:
+                            result = result
+                elif col_of_piece in [1,2,3,4,5,6]:
+                    col = col_of_piece - 1
+                    if self._board[row][col] in [Game.P2Man,Game.P2King]:
+                        result = result
+                    elif self._board[row][col] == Game._EMPTY:
+                        result.append([row,col])
+                    else:
+                        if self._board[row-1][col-1] == Game._EMPTY:
+                            result.append([row-1,col-1])
+                            take += 1
+                        else:
+                            result = result
+                    col = col_of_piece + 1
+                    if self._board[row][col] in [Game.P2Man,Game.P2King]:
+                        result = result
+                    elif self._board[row][col] == Game._EMPTY:
+                        result.append([row,col])
+                    else:
+                        if self._board[row-1][col+1] == Game._EMPTY:
+                            result.append([row-1,col+1])
+                            take += 1
+                        else:
+                            result = result
         else:
-            man_or_king = "K"
-        if row_of_piece not in [1,8]:
-            if self._player == Game.P1:
-                moves.append((row_of_piece + 1, col_of_piece - 1))
-                moves.append((row_of_piece - 1, col_of_piece - 1))
-            else:
-                moves.append((row_of_piece + 1, col_of_piece + 1))
-                moves.append((row_of_piece - 1, col_of_piece + 1))
-        else:
-            if self._player == Game.P1:
-                if row_of_piece == "1":
-                    moves.append((row_of_piece - 1, col_of_piece - 1))
+            row = row_of_piece + 1
+            if col_of_piece == 0:
+                col = col_of_piece + 1
+                if self._board[row][col] in [Game.P2Man,Game.P2King]:
+                    result = result
+                elif self._board[row][col] == Game._EMPTY:
+                    result.append([row,col])
                 else:
-                    moves.append((row_of_piece + 1, col_of_piece - 1))
-            else:
-                if row_of_piece == "1":
-                    moves.append((row_of_piece - 1, col_of_piece + 1))
+                    if self._board[row-1][col+1] == Game._EMPTY:
+                        result.append([row-1,col+1])
+                        take += 1
+                    else:
+                        result = result
+            elif col_of_piece == 7:
+                col = col_of_piece - 1
+                if self._board[row][col] in [Game.P2Man,Game.P2King]:
+                    result = result
+                elif self._board[row][col] == Game._EMPTY:
+                    result.append([row,col])
                 else:
-                    moves.append((row_of_piece + 1, col_of_piece + 1))
-        if man_or_king == "K":
-            if row_of_piece not in [1,8]:
-                if self._player == Game.P1:
-                moves.append((row_of_piece + 1, col_of_piece + 1))
-                moves.append((row_of_piece - 1, col_of_piece + 1))
-            else:
-                moves.append((row_of_piece + 1, col_of_piece - 1))
-                moves.append((row_of_piece - 1, col_of_piece - 1))
-        else:
-            if self._player == Game.P1:
-                if row_of_piece == "1":
-                    moves.append((row_of_piece - 1, col_of_piece + 1))
+                    if self._board[row-1][col-1] == Game._EMPTY:
+                        result.append([row-1,col-1])
+                        take += 1
+                    else:
+                        result = result
+            elif col_of_piece in [1,2,3,4,5,6]:
+                col = col_of_piece - 1
+                if self._board[row][col] in [Game.P2Man,Game.P2King]:
+                    result = result
+                elif self._board[row][col] == Game._EMPTY:
+                    result.append([row,col])
                 else:
-                    moves.append((row_of_piece + 1, col_of_piece + 1))
-            else:
-                if row_of_piece == "1":
-                    moves.append((row_of_piece - 1, col_of_piece - 1))
+                    if self._board[row-1][col-1] == Game._EMPTY:
+                        result.append([row-1,col-1])
+                        take += 1
+                    else:
+                        result = result
+                col = col_of_piece + 1
+                if self._board[row][col] in [Game.P2Man,Game.P2King]:
+                    result = result
+                elif self._board[row][col] == Game._EMPTY:
+                    result.append([row,col])
                 else:
-                    moves.append((row_of_piece + 1, col_of_piece - 1))
-        return moves
+                    if self._board[row-1][col+1] == Game._EMPTY:
+                        result.append([row-1,col+1])
+                        take += 1
+                    else:
+                        result = result
+            if self._board[row_of_piece][col_of_piece] == Game.P2King:
+                row = row_of_piece - 1
+                if col_of_piece == 0:
+                    col = col_of_piece + 1
+                    if self._board[row][col] in [Game.P2Man,Game.P2King]:
+                        result = result
+                    elif self._board[row][col] == Game._EMPTY:
+                        result.append([row,col])
+                    else:
+                        if self._board[row-1][col+1] == Game._EMPTY:
+                            result.append([row-1,col+1])
+                            take += 1
+                        else:
+                            result = result
+                elif col_of_piece == 7:
+                    col = col_of_piece - 1
+                    if self._board[row][col] in [Game.P2Man,Game.P2King]:
+                        result = result
+                    elif self._board[row][col] == Game._EMPTY:
+                        result.append([row,col])
+                    else:
+                        if self._board[row-1][col-1] == Game._EMPTY:
+                            result.append([row-1,col-1])
+                            take += 1
+                        else:
+                            result = result
+                elif col_of_piece in [1,2,3,4,5,6]:
+                    col = col_of_piece - 1
+                    if self._board[row][col] in [Game.P2Man,Game.P2King]:
+                        result = result
+                    elif self._board[row][col] == Game._EMPTY:
+                        result.append([row,col])
+                    else:
+                        if self._board[row-1][col-1] == Game._EMPTY:
+                            result.append([row-1,col-1])
+                            take += 1
+                        else:
+                            result = result
+                    col = col_of_piece + 1
+                    if self._board[row][col] in [Game.P2Man,Game.P2King]:
+                        result = result
+                    elif self._board[row][col] == Game._EMPTY:
+                        result.append([row,col])
+                    else:
+                        if self._board[row-1][col+1] == Game._EMPTY:
+                            result.append([row-1,col+1])
+                            take += 1
+                        else:
+                            result = result
+        return result, take
 
-    def _finished(self):
-        for p in [Game.P1, Game.P2]:
+    def whos_move(self):
+        return "Black to move" if self._player is Game.P1 else "White to move"
+
+    def return_player(self):
+        return self._player
+
+    @property
+    def finished(self):
+        for p in [Game.P1Man, Game.P2Man, Game.P1King, Game.P2King]:
             finished = True
             for row in range(Game._DIM):
                 for col in range(Game._DIM):
                     if self._board[row][col] == p:
                         finished = False
-            if finished == True:
-                return True
-        return None
-        #Do Draw Mechanics later
+            if finished == False:
+                return None
+        if finished == True:
+            return p
     
     def __repr__(self):
-        result = "  " + " ".join(str(i+1) for i in range(Game._DIM))
+        result = "  " + " ".join(str(i+1)+"  " for i in range(Game._DIM))
         for row in range(Game._DIM):
             result += f"\n{row+1} " + "|".join(self._board[row])
             if row != Game._DIM - 1:
--- UI.py
+++ UI.py
@@ -1,4 +1,4 @@
-from Game import Game
+from Game import Game, GameError
 from sys import argv
 
 class GUI:
@@ -14,12 +14,68 @@ class Terminal:
 
 
     def run(self):
-        _ = input("PLAY (Press Any Button to continue, Ctrl + C to stop)")
-        print(self._game)
-        print(self._game._get_legal_moves(3,6))
-        self._game._do_move(3,6,4,5)
-        print(self._game)
-
+        while not self._game.finished:
+            print(self._game)
+            print(self._game.whos_move())
+            try:
+                print("Enter Row and Column of piece to move:")
+                row = int(input("row: "))
+                col = int(input("column: "))
+            except:
+                print("non numeric input")
+                continue
+            if 1 <= row <= 8 and 1 <= col <= 8:
+                try:
+                    legal_moves, takes = self._game._get_legal_moves(row, col)
+                    print("Legal moves for this piece: ")
+                    take_used = False
+                    result = ""
+                    potential_rows = []
+                    potential_columns = []
+                    if takes != 0:
+                        print("There is a take[s] available, which you must do")
+                        take_used = True
+                        current_player = self._game.return_player()
+                        for i in legal_moves:
+                            result = ""
+                            for num in i:
+                                result += str(num + 1)
+                            if current_player == "Black":
+                                if int(result[0]) == int(row - 2):
+                                    print(result[0] + "," + result[1])
+                                    potential_rows.append(int(result[0]))
+                                    potential_columns.append(int(result[1]))
+                            else:
+                                if int(result[0]) == int(row + 2):
+                                    print(result[0] + "," + result[1])
+                                    potential_rows.append(int(result[0]))
+                                    potential_columns.append(int(result[1]))
+                    else:
+                        for i in legal_moves:
+                            result = ""
+                            for num in i:
+                                result += str(num + 1)
+                            print(result[0] + "," + result[1])
+                            potential_rows.append(int(result[0]))
+                            potential_columns.append(int(result[1]))
+                    row_to_move = int(input("Enter row to move to:"))
+                    col_to_move = int(input("Enter col to move to:"))
+                    if row_to_move not in potential_rows:
+                        print("You cannot move there!")
+                        raise ValueError
+                    if col_to_move not in potential_columns:
+                        print("You cannot move there!")
+                        raise ValueError
+                    self._game._do_move(row, col, row_to_move, col_to_move, take_used)
+                except GameError:
+                    print("not your piece to move!")
+                except ValueError:
+                    continue
+            else:
+                print("Row and column must be within 1 - 8")
+        print("Game Finished!")
+        w = self._game.finished
+        print(f"The winner was {w}")
 
 def usage():   
     print(f"""
