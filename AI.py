0768a4ea1ed7a56c3b90cc091aa18a0425052153
Alex Dixon <alexanderdixon267@gmail.com>
2021-10-21 11:41:55 +0100

    commit 21/10

--- AI.py
+++ AI.py
@@ -1,8 +1,12 @@
 from Player import Player
+from Game import Game
 
 class AI(Player):
-    def __init__(self):
-        pass
+    def __init__(self, difficulty, piece):
+        self._difficulty = difficulty
+        self.__game = Game
+        self.__piece = [self.__game.P1Man,self.__game.P1King] if piece == "Black" else [self.__game.P2Man,self.__game.P2King]
 
-    def get_move(self):
+    def get_possible_move(self):
+        # no ideas waht this is even trying to do: for self.__piece in self.__game:
         pass
