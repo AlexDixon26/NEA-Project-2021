820a60d06aab7b9dde05f6c2c8e5a74abf3047b7
Alex Dixon <alexanderdixon267@gmail.com>
2021-11-25 12:22:29 +0000

    another commit for documentation

--- Human.py
+++ Human.py
@@ -1,5 +1,10 @@
 from Player import Player
 
+
 class Human(Player):
+    Users = []
     def __init__(self, User):
         self.__username = User
+        if self.__username not in Human.Users:
+            Human.Users.append(self.__username)
+
