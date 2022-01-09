cd41845a1a56de9e7a8a49118a7c668531bbf3a0
Alex Dixon <alexanderdixon267@gmail.com>
2022-01-09 11:55:56 +0000

    testing databases

---
+++ test.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+
+conn = sqlite3.connect("test.db")
+
+conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+      VALUES (1, 'Paul', 32, 'California', 20000.00 )");
+
+conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+      VALUES (2, 'Allen', 25, 'Texas', 15000.00 )");
+
+conn.commit()
+
+cursor = conn.execute("SELECT id, name, address, salary from COMPANY")
+for row in cursor:
+   print ("ID = ", row[0])
+   print ("NAME = ", row[1])
+   print ("ADDRESS = ", row[2])
+   print ("SALARY = ", row[3], "\n")
